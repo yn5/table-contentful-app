8defcbb472258d22476f710a5b8edcdb2d2d5536
yn5 <ynsnetmail@gmail.com>
2021-12-03 12:56:34 +0100

    fix: build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createClient } from 'contentful-management';
 
 import { FieldExtensionSDK, init, locations } from '@contentful/app-sdk';
 import type { KnownSDK } from '@contentful/app-sdk';
@@ -9,8 +8,8 @@ import '@contentful/forma-36-fcss/dist/styles.css';
 import '@contentful/forma-36-tokens/dist/css/index.css';
 import './index.css';
 
-import Field from './components/Field/Field';
-import LocalhostWarning from './components/LocalhostWarning/LocalhostWarning';
+import { Field } from './components/Field/Field';
+import { LocalhostWarning } from './components/LocalhostWarning/LocalhostWarning';
 
 if (process.env.NODE_ENV === 'development' && window.self === window.top) {
   // You can remove this if block before deploying your app
@@ -21,24 +20,10 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
   init((sdk: KnownSDK) => {
     const root = document.getElementById('root');
 
-    // Creating a CMA client allows you to use the contentful-management library
-    // within your app. See the contentful-management documentation at https://contentful.github.io/contentful-management.js/contentful-management/latest/
-    // to learn what is possible.
-    const cma = createClient(
-      { apiAdapter: sdk.cmaAdapter },
-      {
-        type: 'plain',
-        defaults: {
-          environmentId: sdk.ids.environment,
-          spaceId: sdk.ids.space,
-        },
-      }
-    );
-
     const ComponentLocationSettings = [
       {
         location: locations.LOCATION_ENTRY_FIELD,
-        component: <Field cma={cma} sdk={sdk as FieldExtensionSDK} />,
+        component: <Field sdk={sdk as FieldExtensionSDK} />,
       },
     ];
 
